0719f0f3120dc2789a119732d6e6e578bfb92517
qb2z <huibic@126.com>
2021-11-11 21:21:04 +0800

    Add files via upload

---
+++ keyboard.go
@@ -0,0 +1,24 @@
+//Package keyboard reads user input from the keyboard
+package keyboard
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
+)
+//GetFloat reads a floating-point number from the keyboard
+//It return the number read and any error encountered.
+func GetFloat() (float64, error) {
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		return 0, err
+	}
+	input = strings.TrimSpace(input)
+	number, err := strconv.ParseFloat(input, 64)
+	if err != nil {
+		return 0, err
+	}
+	return number, nil
+}
